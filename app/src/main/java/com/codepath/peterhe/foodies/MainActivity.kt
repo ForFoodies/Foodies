83b87d507f822bdcd1596263a1d8a2107dedb59e
yfdo <dong.yufei@outlook.com>
2022-04-07 02:00:43 -0400

    Updated Profile Fragment and Edit Profile Fragment

--- app/src/main/java/com/codepath/peterhe/foodies/MainActivity.kt
+++ app/src/main/java/com/codepath/peterhe/foodies/MainActivity.kt
@@ -33,31 +33,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         //ParseUser.getCurrentUser().getParseFile("profile")
-       // val image: ParseFile? = ParseUser.getCurrentUser().getParseFile("profile")
+        // val image: ParseFile? = ParseUser.getCurrentUser().getParseFile("profile")
         //Log.i("Main", image?.url.toString())
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;//  set status text dark
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;//  set status text dark
         window.statusBarColor = resources.getColor(R.color.primary_dark)
         getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.light_gray_1))
-       // queryUser(ParseUser.getCurrentUser().objectId)
+        // queryUser(ParseUser.getCurrentUser().objectId)
         val image: ParseFile? = ParseUser.getCurrentUser().getParseFile("profile")
         Log.i("Main", image?.url.toString())
-        Glide.with(this@MainActivity).load(image?.url).override(32,32).apply(
+        Glide.with(this@MainActivity).load(image?.url).override(32, 32).apply(
             RequestOptions().transforms(
                 CenterCrop(), RoundedCorners(10)
-            )).
-        listener(object : RequestListener<Drawable> {
-            override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
+            )
+        ).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                p0: GlideException?,
+                p1: Any?,
+                p2: Target<Drawable>?,
+                p3: Boolean
+            ): Boolean {
                 //Log.e(TAG, "onLoadFailed")
                 //do something if error loading
                 return false
             }
-            override fun onResourceReady(p0: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
+
+            override fun onResourceReady(
+                p0: Drawable?,
+                p1: Any?,
+                p2: Target<Drawable>?,
+                p3: DataSource?,
+                p4: Boolean
+            ): Boolean {
                 //Log.d(TAG, "OnResourceReady")
                 //do something when picture already loaded
                 //setColorStateList()
                 //findViewById<BottomNavigationView>(R.id.bottom_navigation).setItemIconTintList(null)
-                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2).setIconTintMode(PorterDuff.Mode.DST)
-                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2).setIcon(p0)
+                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2)
+                    .setIconTintMode(PorterDuff.Mode.DST)
+                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2)
+                    .setIcon(p0)
                 /*findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(0).getIcon().setColorFilter(
                     getResources().getColor(R.color.app_black), PorterDuff.Mode.SRC_ATOP)
                 findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(1).getIcon().setColorFilter(
@@ -66,8 +81,12 @@ class MainActivity : AppCompatActivity() {
             }
         }).into(findViewById<ImageView>(R.id.iv_profilePlaceHolder))
         val fragmentManager: FragmentManager = supportFragmentManager
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setBackgroundColor(getResources().getColor(R.color.primary))
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {item ->
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setBackgroundColor(
+            getResources().getColor(
+                R.color.primary
+            )
+        )
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener { item ->
             var fragmentToShow: Fragment? = null
             when (item.itemId) {
                 R.id.action_home -> {
@@ -84,7 +103,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             if (fragmentToShow != null) {
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow)
+                    .commit()
             }
             true
         }
@@ -97,7 +117,7 @@ class MainActivity : AppCompatActivity() {
         inflator.inflate(R.menu.appbar, menu)
         return true
     }*/
-    fun queryUser(userId:String) {
+    fun queryUser(userId: String) {
         val query: ParseQuery<ParseUser> = ParseQuery.getQuery(ParseUser::class.java)
         query.include(ParseUser.KEY_OBJECT_ID)
         //query.addDescendingOrder("createdAt")
@@ -108,39 +128,54 @@ class MainActivity : AppCompatActivity() {
             override fun done(user: MutableList<ParseUser>?, e: ParseException?) {
                 if (e != null) {
                     //Log.e(TAG, "Error getting posts")
-                    Toast.makeText(this@MainActivity, "Error getting members", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "Error getting members", Toast.LENGTH_SHORT)
+                        .show()
                 } else {
-                   if (user != null && user.size == 1) {
-                       val image: ParseFile? = user[0].getParseFile("profile")
-                       Log.i("Main", image?.url.toString())
-                       Glide.with(this@MainActivity).load(image?.url).override(32,32).apply(
-                           RequestOptions().transforms(
-                               CenterCrop(), RoundedCorners(10)
-                           )).
-                       listener(object : RequestListener<Drawable> {
-                           override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
-                               //Log.e(TAG, "onLoadFailed")
-                               //do something if error loading
-                               return false
-                           }
-                           override fun onResourceReady(p0: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
-                               //Log.d(TAG, "OnResourceReady")
-                               //do something when picture already loaded
-                               //setColorStateList()
-                               //findViewById<BottomNavigationView>(R.id.bottom_navigation).setItemIconTintList(null)
-                               findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2).setIconTintMode(PorterDuff.Mode.DST)
-                               findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(2).setIcon(p0)
-                               /*findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(0).getIcon().setColorFilter(
-                                   getResources().getColor(R.color.app_black), PorterDuff.Mode.SRC_ATOP)
-                               findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(1).getIcon().setColorFilter(
-                                   getResources().getColor(R.color.app_black), PorterDuff.Mode.SRC_ATOP)*/
-                               return false
-                           }
-                       }).into(findViewById<ImageView>(R.id.iv_profilePlaceHolder))
-                      // val myDrawable: Drawable = findViewById<ImageView>(R.id.iv_profilePlaceHolder).get
-                      // val profile:Drawable = getDrawableFromURL.drawableFromUrl(image?.url)
-                     // findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(R.id.action_profile).setIcon(myDrawable)
-                   }
+                    if (user != null && user.size == 1) {
+                        val image: ParseFile? = user[0].getParseFile("profile")
+                        Log.i("Main", image?.url.toString())
+                        Glide.with(this@MainActivity).load(image?.url).override(32, 32).apply(
+                            RequestOptions().transforms(
+                                CenterCrop(), RoundedCorners(10)
+                            )
+                        ).listener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                p0: GlideException?,
+                                p1: Any?,
+                                p2: Target<Drawable>?,
+                                p3: Boolean
+                            ): Boolean {
+                                //Log.e(TAG, "onLoadFailed")
+                                //do something if error loading
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                p0: Drawable?,
+                                p1: Any?,
+                                p2: Target<Drawable>?,
+                                p3: DataSource?,
+                                p4: Boolean
+                            ): Boolean {
+                                //Log.d(TAG, "OnResourceReady")
+                                //do something when picture already loaded
+                                //setColorStateList()
+                                //findViewById<BottomNavigationView>(R.id.bottom_navigation).setItemIconTintList(null)
+                                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu()
+                                    .getItem(2).setIconTintMode(PorterDuff.Mode.DST)
+                                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu()
+                                    .getItem(2).setIcon(p0)
+                                /*findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(0).getIcon().setColorFilter(
+                                    getResources().getColor(R.color.app_black), PorterDuff.Mode.SRC_ATOP)
+                                findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(1).getIcon().setColorFilter(
+                                    getResources().getColor(R.color.app_black), PorterDuff.Mode.SRC_ATOP)*/
+                                return false
+                            }
+                        }).into(findViewById<ImageView>(R.id.iv_profilePlaceHolder))
+                        // val myDrawable: Drawable = findViewById<ImageView>(R.id.iv_profilePlaceHolder).get
+                        // val profile:Drawable = getDrawableFromURL.drawableFromUrl(image?.url)
+                        // findViewById<BottomNavigationView>(R.id.bottom_navigation).getMenu().getItem(R.id.action_profile).setIcon(myDrawable)
+                    }
                 }
             }
 
