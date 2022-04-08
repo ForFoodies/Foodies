91dd0890c20946f167be07f0f745eb00242fba9d
PeterHe0226 <peterhe@umich.edu>
2022-04-08 12:33:28 -0400

    Map for group members partially done! Need member profile to update more

--- app/src/main/java/com/codepath/peterhe/foodies/MainActivity.kt
+++ app/src/main/java/com/codepath/peterhe/foodies/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.codepath.peterhe.foodies
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,6 +16,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -28,8 +34,9 @@ import com.codepath.peterhe.foodies.fragments.UserProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var appbar: Toolbar
+    private lateinit var locationManager: LocationManager
     private lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -113,6 +120,7 @@ class MainActivity : AppCompatActivity() {
         }
         // Set default selection
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+        getLocation()
     }
 
     fun queryUser(userId: String) {
@@ -188,6 +196,63 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        saveCurrentUserLocation()
+    }
+
+    private fun saveCurrentUserLocation() {
+        // requesting permission to get user's location
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        } else {
+            // getting last know user's location
+            val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+            // checking if the location is null
+            if (location != null) {
+                // if it isn't, save it to Back4App Dashboard
+                val currentUserLocation = ParseGeoPoint(location.latitude, location.longitude)
+                val currentUser = ParseUser.getCurrentUser()
+                if (currentUser != null) {
+                    currentUser.put("Location", currentUserLocation)
+                    currentUser.saveInBackground()
+                } else {
+                    // do something like coming back to the login activity
+                }
+            } else {
+                // if it is null, do something like displaying error and coming back to the menu activity
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        try {
+            locationManager =
+                Parse.getApplicationContext().getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                5000,
+                5.toFloat(),
+                this
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
 
