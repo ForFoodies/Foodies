6af55e3180db5f391b49c2eee6f5e1ee73b7f58e
Yuni <syjeong215@gmail.com>
2022-04-08 23:48:29 -0400

    landing page styling

--- app/src/main/java/com/codepath/peterhe/foodies/LogInActivity.kt
+++ app/src/main/java/com/codepath/peterhe/foodies/LogInActivity.kt
@@ -3,7 +3,9 @@ package com.codepath.peterhe.foodies
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import com.google.android.material.button.MaterialButton
 import com.parse.ParseUser
 
 class LogInActivity : AppCompatActivity() {
@@ -14,13 +16,15 @@ class LogInActivity : AppCompatActivity() {
         if (ParseUser.getCurrentUser() != null) {
             gotoMainActivity()
         }
-        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.primary));
+        // transparent status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         getSupportActionBar()?.hide()
-        findViewById<ImageButton>(R.id.redirect_signin).setOnClickListener{
+
+        findViewById<MaterialButton>(R.id.redirect_signin).setOnClickListener{
             val intent = Intent(this,SignInActivity::class.java)
             startActivity(intent)
         }
-        findViewById<ImageButton>(R.id.redirect_signup).setOnClickListener{
+        findViewById<MaterialButton>(R.id.redirect_signup).setOnClickListener{
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
