fcad879069bed7a9485cdf44dcce2b56d0927246
Yuni <syjeong215@gmail.com>
2022-04-08 21:28:02 -0400

    merge error fixed

--- app/src/main/java/com/codepath/peterhe/foodies/fragments/GroupChatListFragment.kt
+++ app/src/main/java/com/codepath/peterhe/foodies/fragments/GroupChatListFragment.kt
@@ -5,7 +5,6 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
